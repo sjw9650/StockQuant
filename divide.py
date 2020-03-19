42a2faf207d23746ea3bd57094ff18f3c32389e3
jinwook <sjw9650@gmail.com>
2020-03-20 06:46:26 +0900

    fix some error

--- divide.py
+++ divide.py
@@ -7,12 +7,6 @@ import copy
 
 class DividStock(object) :
 
-    creon = CreonApi.Creon()
-    codeinformList = [0, 4, 17,73]
-    stockList = []
-    # 0 종목코드, 4 현재가, 17 종목명, 20 : 총 상장주식수, 74 배당률, 75 부채비율, 77 ROA, 80 순이익증가율
-    # 89 BPS, 91 영업이익 95 결산연월, 102 분기영업이익,107 분기 ROE, 111 결산연월  123 SPS 124 CFPS
-
     def __init__(self, stockCode, price, stockName,dividend):
         print("divide")
         self.stockCode = stockCode
@@ -20,12 +14,20 @@ class DividStock(object) :
         self.stockName = stockName
         self.dividend =dividend
 
+class DivideStrategy():
+
+    creon = CreonApi.Creon()
+    codeinformList = [0, 4, 17,73]
+    stockList = []
+    # 0 종목코드, 4 현재가, 17 종목명, 20 : 총 상장주식수, 74 배당률, 75 부채비율, 77 ROA, 80 순이익증가율
+    # 89 BPS, 91 영업이익 95 결산연월, 102 분기영업이익,107 분기 ROE, 111 결산연월  123 SPS 124 CFPS
+
     def refinedStock(self, inputdata):
         data = []
         lenofdata = len(inputdata)
         for index in range(lenofdata):
             tempdata = copy.deepcopy(inputdata[index])
-            tempStock = Stock(tempdata[0],tempdata[1],tempdata[2],tempdata[3])
+            tempStock = DividStock(tempdata[0],tempdata[1],tempdata[2],tempdata[3])
             self.stockList.append(tempStock)
 
     def saveDataStockList(self):
@@ -45,8 +47,9 @@ class DividStock(object) :
         else:
             print("bye!")
 
+
 def DivideStockList():
-    divideStcok = DividStock()
+    divideStcok = DivideStrategy()
     divideStcok.dividStrategy()
 
 DivideStockList()
